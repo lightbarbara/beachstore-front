e3671b1f691a107164742fc1fb291d3b8386702f
Lucas Lacerda Buettel <buettellucas@gmail.com>
2022-11-24 01:27:36 -0300

    feat: first commit SignIn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { GlobalStyle } from "./assets/GlobalStyle";
+import SignIn from "./components/SignIn";
+/* import SignUp from "./SignUp"; */
+
+
 export default function App() {
-  return (
-    <>
-    </>
-  );
+    return (
+
+        <BrowserRouter>
+            <GlobalStyle />
+            <Routes>
+                <Route path="/" element={<SignIn />} />
+                {/* <Route path="/signUp" element={<SignUp/>} /> */}
+                
+
+            </Routes>
+        </BrowserRouter>
+
+    )
 }
