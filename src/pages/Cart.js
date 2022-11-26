6722c9b5c69e2f3337b6104a2b655456e3703c5c
Barbara Luz <barbaraluz@eq.ufrj.br>
2022-11-26 16:56:51 -0300

    feat: top bar

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,20 @@
+import styled from "styled-components"
+import TopBar from "../components/TopBar"
+
 export default function Cart() {
 
-    return(
-        <>
-        </>
+    return (
+        <Container>
+            <TopBar opcao='produtos' />
+        </Container>
     )
 }
+
+const Container = styled.div`
+position: relative;
+width: 100vw;
+height: 100vh;
+padding: 0 5vw;
+padding-top: 90px;
+background-color: #D3E3E2;
+`
